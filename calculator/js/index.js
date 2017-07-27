590f0583788b2aaa49b51fda67514f477c521371
caikk <caikk@vastio.com>
2017-07-27 17:14:27 +0800

    样式修改

--- calculator/js/index.js
+++ calculator/js/index.js
@@ -1,6 +1,15 @@
 (function() {
-	//"use strict";
+	"use strict";
 	var swiper = new Swiper('.swiper-container',{
 		pagination: '.swiper-pagination'
+	});
+
+	$(".enter").on("click",function() {
+		$(".swiper-container").remove();
+		$(".main-content").show("slow");
+	});
+
+	$(".number").on("click",function() {
+		$(this).css({"background":"#B2B2B2"});
 	});
 }());

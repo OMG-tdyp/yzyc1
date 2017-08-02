7ef07102cdbbb1438d52209d3ed1c249e3dc5b3f
ckk <497990981@qq.com>
2017-08-02 23:12:28 +0800

    继续优化代码

--- calculator/js/index.js
+++ calculator/js/index.js
@@ -10,19 +10,33 @@
 	});
 
 	var num1 = 0;
-	var num2 = 0;
+	var num2;
 	var tmp1;
 	var tmp2;
 	var tmpop = "＝";
 	var status = 0;//0：还在一次计数中 1：重新开始计数
-	var count = 0;//0：第一次点击运算符 1：非第一次点击运算符
+	var start = 0;//0：没选取过数字 1：选取过数字
 	const numbtn = $(".number");//数字按钮
 	const tmpbtn = $(".result span");//显示结果区
 	const opbtn = $(".operator");//运算符按钮
+	var defaultNum = localStorage.getItem("default");
+	defaultNum = defaultNum || 0;
+	(defaultNum === "错误") ? initNum(0) : initNum(1);
+
+	//本地存储，默认显示
+	function initNum(flag) {
+		(flag === 0) ? tmpbtn.html(defaultNum) : tmpbtn.html(parseInt(defaultNum));
+	}
+	//关闭刷新页面时保存当前值
+	window.onbeforeunload = (e) => {
+		localStorage.setItem("default",tmpbtn.html());
+	}
 
 	//显示当前数字
 	function nowNum() {
 		const self = $(this);
+		start = 1;
+		opbtn.removeClass('active');
 		(status === 0) ? count1(self) : count2(self);
 	}
 
@@ -54,9 +68,13 @@
 
 	//清零
 	$(".clear").on("click",function() {
+		localStorage.setItem("default",0);
+		start = 0;
 		status = 0;
-		count = 0;
 		tmp1 = 0;
+		tmp2 = 0;
+		num1 = 0;
+		num2 = 0;
 		tmpbtn.html(tmp1);
 		numbtn.removeClass('current');
 		opbtn.removeClass('active');
@@ -64,69 +82,54 @@
 
 	//加法
 	var addition = (a,b) => {
-		a = a || 0;
-		b = b || 0;
 		let n;
-		count = 1;
 		(a === "错误" || b === "错误") ? n = "错误" : n = a + b;
 		return n;
 	}
 	//减法
 	var subtraction = (a,b) => {
-		a = a || 0;
-		b = b || 0;
 		let n;
-		count = 1;
 		(a === "错误" || b === "错误") ? n = "错误" : n = a - b;
 		return n;
 	}
 	//乘法
 	var multiplication = (a,b) => {
-		a = a || 0;
-		b = b || 1;
 		let n;
-		count = 1;
+		b = b || 1;
 		(a === "错误" || b === "错误") ? n = "错误" : n = a * b;
 		return n;
 	}
 	//除法
 	var division = (a,b) => {
-		a = a || 0;
-		b = b || 1;
 		let n;
-		count = 1;
-		(b === 0 || a === "错误" || b === "错误") ? n = "错误" : n = a / b;
-		return n;
+		(start === 0) ? n = 0 : (b = b || 1,(b === 0 || a === "错误" || b === "错误") ? n = "错误" : n = a / b);
+		return n;		
 	}
 	//等于
 	var equal = (a,b) => {
-		a = a || 0;
-		b = a;
-		let n;
-		count = 1;
-		(a === "错误" || b === "错误") ? n = "错误" : n = b;
+		let n = tmpbtn.html();
+		(n === "错误") ? "" : n = parseInt(n);
 		return n;
 	}
 
 	//点击运算符
 	opbtn.on("click",function() {
 		tmp1 = num1;
-		tmp2 = num2;
 		switch(tmpop) {
 			case "＋":
-				num1 = addition(tmp1,tmp2);
+				num1 = addition(tmp1,num2);
 				break;
 			case "－":
-				num1 = subtraction(tmp1,tmp2);
+				num1 = subtraction(tmp1,num2);
 				break;
 			case "×":
-				num1 = multiplication(tmp1,tmp2);
+				num1 = multiplication(tmp1,num2);
 				break;
 			case "÷":
-				num1 = division(tmp1,tmp2);
+				num1 = division(tmp1,num2);
 				break;
 			case "＝":
-				num1 = equal(tmp1,tmp2);
+				num1 = equal(tmp1,num2);
 				break;
 		}	
 		tmpbtn.html(num1);	
@@ -135,7 +138,6 @@
 		$(this).addClass('active').siblings().removeClass('active');
 		const type = $(this).html();
 		tmpop = type; 
-		status = 1;
-		
+		status = 1;		
 	});
 }());
